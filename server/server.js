ae1a6c53f87cba93b76b139fef96cc3b9478df6d
jsong73 <jessicajinsong@gmail.com>
2022-12-21 10:07:21 -0500

    working on creating database

--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
+//importing express library
 const express = require("express");
-
 const app = express();
-
+//importing the connection.js file
+const sequelize = require("./config/connection");
+//connect to the available port if not specified use port 3000
 const PORT = process.env.PORT || 3003;
 
-app.listen(PORT, (err) => console.log(`listening to port ${PORT}`));
+
+//to connect to port using express
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, (err) => console.log(`listening to port ${PORT}`));
+})
+
