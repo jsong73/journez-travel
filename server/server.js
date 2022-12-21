7fc22004abebda14627f1d01bfa74ec05e844587
jsong73 <jessicajinsong@gmail.com>
2022-12-21 10:12:50 -0500

    creating seeds file

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3003;
 
 
 //to connect to port using express
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, (err) => console.log(`listening to port ${PORT}`));
 })
 
