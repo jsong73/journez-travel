dad3a9117dc2568a87358f0a9a33ce4ed11ecf69
jsong73 <jessicajinsong@gmail.com>
2022-12-22 18:26:12 -0500

    created flight api route

--- server/controllers/apiRoutes/flightRoutes.js
+++ server/controllers/apiRoutes/flightRoutes.js
@@ -1,2 +1,25 @@
 const router = require("express").Router();
-const { Flight } = require("../../")
+const { Flight } = require("../../models");
+
+//api/flight
+router.post("/", async (req, res) => {
+    try{
+        const flightData = await Flight.create(req.body)
+        res.status(200).json({ msg: "Flight details was successfully created!", flightData })
+    } catch(err){
+        res.status(400).json(err)
+    }
+});
+
+router.get("/", async (req, res) => {
+    try{
+        const flightData = await Flight.findAll();
+        const flight = flightData.map((data) => data.get({ plain: true }));
+        res.status(200).json(flight);
+        console.log(flight);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+});
+
+module.exports = router;
