814a3ee5dc49ed28c6df52aa3db0d9f6d5f5b6f8
jsong73 <jessicajinsong@gmail.com>
2022-12-22 13:17:56 -0500

    working on index.js api route

---
+++ server/controllers/apiRoutes/index.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const tripRoutes = require("./tripRoutes");
+const accountRoutes = require("./accountRoutes");
+const activityRoutes = require("./activityRoutes");
+const flightRoutes = require("./flightRoutes");
+const hotelRoutes = require("./hotelRoutes");
+const restaurantRoutes = require("./restaurantRoutes");
+const transportationRoutes = require("./transportationRoutes");
+
+router.use("/account", accountRoutes);
+router.use("/trips", tripRoutes);
+router.use("/activity", activityRoutes);
+router.use("/flight", flightRoutes);
+router.use("/hotel", hotelRoutes);
+router.use("/restaurant", restaurantRoutes);
+router.use("/transportation", transportationRoutes);
