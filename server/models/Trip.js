f39e63194a4437821a0bc6d2e5cb019e1eb9326d
jsong73 <jessicajinsong@gmail.com>
2022-12-27 19:02:46 -0500

    completed trip model

--- server/models/Trip.js
+++ server/models/Trip.js
@@ -1,1 +1,37 @@
 const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const tripSchema = new Schema(
+    {
+        tripName: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        description: {
+            type: String,
+            required: true,
+            maxlength: 280,
+            minlength: 1,
+        },
+        location: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        startDate: {
+            type: Date,
+            default: Date.now,
+            get: (timestamp) => dateFormat(timestamp),
+        },
+        endDate: {
+            type: Date,
+            deafult: Date.now,
+            get: (timestamp) => dateFormat(timestamp),
+        },
+    }
+)
+
+const Trip = model("Trip", tripSchema);
+
+module.exports = Trip;
