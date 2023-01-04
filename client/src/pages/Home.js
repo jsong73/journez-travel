2b1809bb9b89d2c6b3d5d0fa66368a6705426d02
jsong73 <jessicajinsong@gmail.com>
2023-01-04 13:35:32 -0500

    working on home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,28 @@
 import React from "react";
+import Auth from "../utils/auth";
+import { Link } from "react-router-dom";
+import Background from "../images/travelhome.jpg"
 
 const Home = () => {
-
+    const styles = {
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        width: "100vh"
+    }
     return(
         <div>
-           <h1> journEZ </h1>
+            <img src={Background} alt="background" style= {styles}></img>
+            {Auth.loggedIn() ? (
+            <>
+           <Link to="/home"> Create a trip </Link>    
+
+            </>
+            ) : (
+            <>
+            <Link to="/home"> Get started </Link>    
+  
+        </>
+        )}
         </div>
     )
 }
