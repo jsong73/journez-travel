47cb7324014b897e259f2b51743f2a694ce06235
jsong73 <jessicajinsong@gmail.com>
2023-01-10 11:07:39 -0500

    client package.json error

--- server/server.js
+++ server/server.js
@@ -8,24 +8,24 @@ const path = require("path");
 const PORT = process.env.PORT || 3003;
 const { ApolloServer } = require("apollo-server-express");
 const { typeDefs, resolvers } = require("./schemas");
-// const { authMiddleware }= require("./utils/auth");
+const { authMiddleware }= require("./utils/auth");
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  // context: authMiddleware,
+  context: authMiddleware,
 });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 
 //add below two lines for deployment
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 
 const startApolloServer = async (typeDefs, resolvers) => {
