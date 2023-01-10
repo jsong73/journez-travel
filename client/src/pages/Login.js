fe93e7f688e5d610fb2c7ec7ab6973543b8d41ac
jsong73 <jessicajinsong@gmail.com>
2023-01-10 18:28:43 -0500

    worked on login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -43,29 +43,26 @@ const Login = (props) => {
     return(
         <div className="h-screen">
             <div className="container px-6 py-12 h-full">
-            <div class="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
-            <div class="md:w-8/12 lg:w-6/12 mb-12 md:mb-0">
+            <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+            <div className="md:w-8/12 lg:w-6/12 mb-12 md:mb-0">
             
             <img 
             className="w-full"
+    
             src={LoginPicture} 
             alt="login-pic" />
             </div>
-            
-            <h1 className="font-medium text-3xl leading-snug uppercase"> Sign in</h1>
-
-            <div> Or{' '}
-             <Link to="/signup"> create a new account </Link>
-            </div>
-
-         {data ? (
-                <div>
-                You are now logged in. You will now be directed to the homepage.
-                </div>
-            ) : (
-        <div class="md:w-8/12 lg:w-5/12 lg:ml-20">
+     
+         <div className="md:w-8/12 lg:w-5/12 lg:ml-20">
+            <h1 className="mb-4 font-medium text-3xl leading-snug uppercase">Sign In</h1>
+                   {data ? (
+                         <div>
+                          You are now logged in. You will now be directed to the homepage.
+                         </div>
+                        ) : (
+   
             <form onSubmit= {loginFormHandler}>
-                <div class="mb-6">
+                <div className="mb-6">
                     <input
                         className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                         placeholder="Email"
@@ -75,7 +72,7 @@ const Login = (props) => {
                         value= {formState.email}
                         onChange={handleChange}/>
                 </div>
-                <div class="mb-6">
+                <div className="mb-6">
                     <input
                         className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                         placeholder="Password"
@@ -85,14 +82,19 @@ const Login = (props) => {
                         value= {formState.password}
                         onChange={handleChange}/>
                 </div>
-                <button 
-                className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full" 
-                type="submit"> Sign in </button>    
+                    <button 
+                        className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full" 
+                         type="submit"> Sign in </button>    
+
+                <div class="text-sm font-semibold mt-3 pt-1 mb-0"> Don't have an account?
+                        <a class="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"> <Link to="/signup">Register</Link> 
+                        </a>
+                </div>
                  
             </form>
-            </div>
+           
             )}
-
+        </div>
         {error && (
           <div>{error.message}</div>
         )}
