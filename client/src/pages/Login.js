254f662372f50e4841374717fdf20cd4dd48a2a3
jsong73 <jessicajinsong@gmail.com>
2023-01-10 18:11:07 -0500

    install tailwind

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,6 +3,7 @@ import { LOGIN_USER } from "../utils/mutations";
 import { useMutation } from "@apollo/client";
 import Auth from "../utils/auth";
 import { Link } from "react-router-dom";
+import LoginPicture from "../images/Login.jpg";
 
 const Login = (props) => {
     const [formState, setFormState] = useState({
@@ -40,8 +41,18 @@ const Login = (props) => {
     };
 
     return(
-        <div>
-            <h1>Sign in</h1>
+        <div className="h-screen">
+            <div className="container px-6 py-12 h-full">
+            <div class="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+            <div class="md:w-8/12 lg:w-6/12 mb-12 md:mb-0">
+            
+            <img 
+            className="w-full"
+            src={LoginPicture} 
+            alt="login-pic" />
+            </div>
+            
+            <h1 className="font-medium text-3xl leading-snug uppercase"> Sign in</h1>
 
             <div> Or{' '}
              <Link to="/signup"> create a new account </Link>
@@ -52,32 +63,42 @@ const Login = (props) => {
                 You are now logged in. You will now be directed to the homepage.
                 </div>
             ) : (
+        <div class="md:w-8/12 lg:w-5/12 lg:ml-20">
             <form onSubmit= {loginFormHandler}>
-                <input
-                    placeholder="Email"
-                    name="email"
-                    type="email"
-                    autoComplete="off"
-                    value= {formState.email}
-                    onChange={handleChange}/>
-        
-                <input
-                    placeholder="Password"
-                    name="password"
-                    type="password"
-                    autoComplete="off"
-                    value= {formState.password}
-                    onChange={handleChange}/>
-        
-                <button type="submit"> Sign in </button>     
+                <div class="mb-6">
+                    <input
+                        className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                        placeholder="Email"
+                        name="email"
+                        type="email"
+                        autoComplete="off"
+                        value= {formState.email}
+                        onChange={handleChange}/>
+                </div>
+                <div class="mb-6">
+                    <input
+                        className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                        placeholder="Password"
+                        name="password"
+                        type="password"
+                        autoComplete="off"
+                        value= {formState.password}
+                        onChange={handleChange}/>
+                </div>
+                <button 
+                className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full" 
+                type="submit"> Sign in </button>    
+                 
             </form>
+            </div>
             )}
 
         {error && (
           <div>{error.message}</div>
         )}
-
         </div>
+        </div>
+    </div>
     );
 };
 
