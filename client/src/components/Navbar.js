d75a0f49bc747a00c06500d3e2393a6c1bf97d8c
jsong73 <jessicajinsong@gmail.com>
2023-01-12 20:20:00 -0500

    nav

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,17 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Auth from "../utils/auth";
 
+
 const Navbar = () => {
     const logout = (event) => {
         event.preventDefault();
         Auth.logout();
 }
-
-
     return(
   
-        <nav className="text-md font-normal leading-snug relative w-full flex flex-wrap items-center justify-between py-5 bg-gray-100 text-gray-800 hover:text-gray-900 focus:text-gray-900 shadow-lg navbar navbar-expand-lg navbar-light">
-            <div className="container-fluid w-full flex flex-wrap items-center justify-between px-10">
+        <nav>
+            <div>
                 {Auth.loggedIn() ? (
                 <>
                     <Link to="/home"> Home </Link>
@@ -24,16 +23,17 @@ const Navbar = () => {
                 ) : (
                  <>
         
-                    <ul className= "navbar-nav flex pl-0 list-style-none mr-auto">
-                        <li className=" nav-item p-2"> 
-                            <div> <Link to="/home"> Home </Link> </div> 
+                    <ul className= "flex items-center justify-center h-full">
+                        <li> 
+                            <button> <Link to="/home"> Home </Link> </button> 
                         </li>  
-                        <li className="nav-item p-2">
-                            <div> <Link to="/"> Log in </Link> </div>
+                        <li>
+                            <button> <Link to="/"> Log in </Link> </button>
                         </li>      
-                       <li className="nav-item p-2">
-                          <div> <Link to="/signup"> Sign up </Link> </div>
+                       <li>
+                          <button> <Link to="/signup"> Sign up </Link> </button>
                         </li>
+  
                     </ul>
 
                 </>
