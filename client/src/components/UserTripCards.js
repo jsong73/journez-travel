1d78f7677736220e982544a9b7478d29cf520cba
jsong73 <jessicajinsong@gmail.com>
2023-01-18 22:20:16 -0500

    working on user trip card css

--- client/src/components/UserTripCards.js
+++ client/src/components/UserTripCards.js
@@ -4,25 +4,24 @@ import DeleteTrip from "./DeleteTrip";
 import UpdateTrip from "./UpdateTrip"
 import Auth from "../utils/auth";
 
-
 const UserTripCards = ({ trips }) => {
 // console.log(trips)
 
 if(!trips.length){
     return <div className="mt-4 text-md tracking-tight font-medium text-red-700" role="alert"> No trips booked as of yet! </div>
 }
-
     return(
-        <div>
+         <div className="grid grid-cols-4 justify-center items-center flex-wrap">
             {trips.map((trip) => (
                 <div key={trip._id}>
+                    
+                 <div className="block max-w-sm p-6 mb-5 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-white-800 dark:border-white-700        dark:hover:bg-white-700">
                     <Link to={`/trips/${trip._id}`}>
 
-                    <div> 
                     <DeleteTrip 
                     tripId ={trip._id} 
                     isLoggedInUser= {Auth.loggedIn() === true }/> 
-                    </div>
+                
 
                     <h1 className="italic underline text-3xl font-bold tracking-tight text-gray-700"> {trip.tripName}</h1>
                     </Link>
@@ -42,10 +41,9 @@ if(!trips.length){
                     startDate={trip.startDate}
                     endDate={trip.endDate}
                     />
-                   
-
 
                     </div>
+                </div>
             ))}
        
         </div>
