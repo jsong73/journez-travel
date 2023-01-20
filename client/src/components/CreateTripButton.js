1bc54e6f558ff95ad104f5bf4cca10a4bbdc53b5
jsong73 <jessicajinsong@gmail.com>
2023-01-20 12:15:34 -0500

    fixed date issue

--- client/src/components/CreateTripButton.js
+++ client/src/components/CreateTripButton.js
@@ -21,9 +21,14 @@ const CreateTripButton = ({ formState }) => {
     };
 
     return (
+<div>
         <button 
-            className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full" 
+            className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading- 
+            snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 
+            focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg 
+            transition duration-150 ease-in-out w-full" 
             onClick={tripFormHandler}> Create trip </button>
+</div> 
     )
 }
 
--- server/utils/dateFormat.js
+++ server/utils/dateFormat.js
@@ -2,19 +2,19 @@ const addDateSuffix = (date) => {
     let dateStr = date.toString();
   
     // get last char of date string
-    const lastChar = dateStr.charAt(dateStr.length - 1);
+  //   const lastChar = dateStr.charAt(dateStr.length - 1);
   
-    if (lastChar === '1' && dateStr !== '11') {
-      dateStr = `${dateStr}st`;
-    } else if (lastChar === '2' && dateStr !== '12') {
-      dateStr = `${dateStr}nd`;
-    } else if (lastChar === '3' && dateStr !== '13') {
-      dateStr = `${dateStr}rd`;
-    } else {
-      dateStr = `${dateStr}th`;
-    }
+  //   if (lastChar === '1' && dateStr !== '11') {
+  //     dateStr = `${dateStr}st`;
+  //   } else if (lastChar === '2' && dateStr !== '12') {
+  //     dateStr = `${dateStr}nd`;
+  //   } else if (lastChar === '3' && dateStr !== '13') {
+  //     dateStr = `${dateStr}rd`;
+  //   } else {
+  //     dateStr = `${dateStr}th`;
+  //   }
   
-    return dateStr;
+    return dateStr ;
   };
   
   // function to format a timestamp, accepts the timestamp and an `options` object as parameters
@@ -42,7 +42,7 @@ const addDateSuffix = (date) => {
     const formattedMonth = months[dateObj.getMonth()];
   
     const dayOfMonth = dateSuffix
-      ? addDateSuffix(dateObj.getDate())
+      ? addDateSuffix(dateObj.getDate()+1)
       : dateObj.getDate();
   
     const year = dateObj.getFullYear();
