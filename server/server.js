c33b9a5de6f8a7eb38936f07f7d8071307fa9cbd
jsong73 <jessicajinsong@gmail.com>
2023-01-22 21:25:34 -0500

    deployment lines

--- server/server.js
+++ server/server.js
@@ -19,13 +19,13 @@ const server = new ApolloServer({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 
-//add below two lines for deployment
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
+// add below two lines for deployment
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 
 const startApolloServer = async (typeDefs, resolvers) => {
